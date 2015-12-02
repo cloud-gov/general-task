a21478885e603a743cea7c3e56cb010c0013566e
M. Adam Kendall <michael.kendall@gsa.gov>
2015-12-02 17:17:23 -0500

    Update tests so that grep match can happen at the start of the version output

--- lib/test_command.sh
+++ lib/test_command.sh
@@ -8,7 +8,7 @@ function test_command {
   local version_options="${3:---version}"
 
   echo -n "it should expose the $name command... "
-   $name $version_options 2>&1 | grep " $version" > /dev/null
+   $name $version_options 2>&1 | grep -E "( |^)$version" > /dev/null
   [ "$?" -ne 0 ] && echo fail && exit 1
   echo pass
 }
