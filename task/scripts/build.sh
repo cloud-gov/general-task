38e4c26aabbafb599bea178636cb591aab850ab5
M. Adam Kendall <michael.kendall@gsa.gov>
2015-12-02 17:18:21 -0500

    Add bosh cli to the image

--- task/scripts/build.sh
+++ task/scripts/build.sh
@@ -18,13 +18,23 @@ apt-get -y install build-essential="$BUILD_ESSENTIAL_VERSION" \
                    unzip="$UNZIP_VERSION" \
                    curl="$CURL_VERSION" \
                    git="$GIT_VERSION" \
+                   ruby2.0="$RUBY_VERSION" \
+                   ruby2.0-dev="$RUBY_DEV_VERSION" \
+                   libmysqlclient-dev="$LIBMYSQLCLIENT_DEV_VERSION" \
                    libpopt-dev="$LIBPOPT_DEV_VERSION" \
+                   libpq-dev="$LIBPQ_DEV_VERSION" \
                    libssl-dev="$LIBSSL_DEV_VERSION" \
                    libcurl4-openssl-dev="$LIBCURL4_OPENSSL_DEV_VERSION" \
                    libxslt1-dev="$LIBXSLT1_DEV_VERSION" \
                    libyaml-dev="$LIBYAML_DEV_VERSION"
+# Set default versions of ruby and gem to 2.0 versions
+update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby2.0 1
+update-alternatives --install /usr/bin/gem gem /usr/bin/gem2.0 1
 
 echo "4. Installing Spiff"
 curl -L -o /tmp/spiff.zip "https://github.com/cloudfoundry-incubator/spiff/releases/download/v$SPIFF_RELEASE_VERSION/spiff_linux_amd64.zip"
 unzip /tmp/spiff.zip -d /usr/local/bin
 rm -f /tmp/spiff.zip
+
+echo "5. Installing BOSH CLI"
+gem2.0 install bosh_cli -v "$BOSH_CLI_RELEASE_VERSION" --no-ri --no-rdoc
