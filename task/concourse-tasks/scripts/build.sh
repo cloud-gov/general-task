dec8231963833c42317c2c0e5c954eca5729bfdc
Van Nguyen <vanh.nguyen@gsa.gov>
2022-11-04 11:12:37 -0400

    not using config file

--- task/concourse-tasks/scripts/build.sh
+++ task/concourse-tasks/scripts/build.sh
@@ -27,7 +27,7 @@ apt-get -y -q install \
   wget \
 
 echo "UA attaching"
-ua attach --attach-config ua-attach-config.yaml
+ua attach $TOKEN
 
 echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list
 wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | apt-key add -
