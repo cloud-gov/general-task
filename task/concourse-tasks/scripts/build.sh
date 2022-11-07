fcfcd78875e25034d1141be2a8f91fbf940de4bf
Van Nguyen <vanh.nguyen@gsa.gov>
2022-11-07 10:13:04 -0500

    Activate the hardening script

--- task/concourse-tasks/scripts/build.sh
+++ task/concourse-tasks/scripts/build.sh
@@ -187,6 +187,10 @@ chmod a+x /usr/bin/uaa
 echo "Installing grype cli"
 curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sh -s -- -b /usr/local/bin
 
+echo "UA hardening"
+/usr/share/ubuntu-scap-security-guides/cis-hardening/Canonical_Ubuntu_18.04_CIS-harden.sh lvl1_server
+
+
 echo "Cleaning up ua"
 apt-get purge --auto-remove -y \
   ubuntu-advantage-tools && \
