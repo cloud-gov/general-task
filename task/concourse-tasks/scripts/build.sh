c62c432af9a753e3aca292f8645ffcd7375a0edf
Van Nguyen <vanh.nguyen@gsa.gov>
2022-11-09 10:34:33 -0500

    commented out hardening to get a baseline

--- task/concourse-tasks/scripts/build.sh
+++ task/concourse-tasks/scripts/build.sh
@@ -188,7 +188,7 @@ echo "Installing grype cli"
 curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sh -s -- -b /usr/local/bin
 
 echo "UA hardening"
-/usr/share/ubuntu-scap-security-guides/cis-hardening/Canonical_Ubuntu_18.04_CIS-harden.sh lvl1_server
+##/usr/share/ubuntu-scap-security-guides/cis-hardening/Canonical_Ubuntu_18.04_CIS-harden.sh lvl1_server
 
 
 echo "Cleaning up ua"
