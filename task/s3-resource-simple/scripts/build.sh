0d64b1cd1f43bd0f96d62dae7e0dfbf8b4c6acef
David Anderson <david.n.anderson@gsa.gov>
2023-02-01 14:53:10 -0500

    testing using script for s3 update

---
+++ task/s3-resource-simple/scripts/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+#
+# Source configuration environment variables
+#
+source ./config.sh
+
+echo "Configuring ua attach config"
+cat <<EOF >> ua-attach-config.yaml
+token: $TOKEN
+enable_services:
+- cis
+- esm-infra
+
+EOF
+
+apt-get update
+apt-get -y -q install \
+  ubuntu-advantage-tools ca-certificates \
+
+echo "UA attaching"
+ua attach --attach-config ua-attach-config.yaml
+
+echo "Installing grype cli"
+curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sh -s -- -b /usr/local/bin
+
+echo "UA hardening"
+/usr/share/ubuntu-scap-security-guides/cis-hardening/Canonical_Ubuntu_18.04_CIS-harden.sh lvl1_server
+
+
+echo "Cleaning up ua"
+apt-get purge --auto-remove -y \
+  ubuntu-advantage-tools && \
+  rm -rf /var/lib/apt/lists/*
