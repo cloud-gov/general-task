7cdd9052b059182104b51da0bb84a0e5e8c1f0ca
David Anderson <david.n.anderson@gsa.gov>
2023-02-03 14:34:39 -0500

    adding other tools

--- task/s3-resource-simple/scripts/build.sh
+++ task/s3-resource-simple/scripts/build.sh
@@ -15,6 +15,11 @@ apt-get update
 apt-get -y -q install \
   ubuntu-advantage-tools ca-certificates \
   curl \
+  tzdata \
+  wget \
+
+echo "Updating system timezone"
+ln -sf "/usr/share/zoneinfo/$SYSTEM_TIMEZONE" /etc/localtime
 
 echo "UA attaching"
 ua attach --attach-config ua-attach-config.yaml
