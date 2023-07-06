75b8dcb873644fb15d9bcd9c5c1b771a6b944ab5
David Anderson <david.n.anderson@gsa.gov>
2023-07-06 09:50:00 -0400

    fix formatting

--- task/harden-concourse-tasks/scripts/build.sh
+++ task/harden-concourse-tasks/scripts/build.sh
@@ -94,11 +94,11 @@ apt-get -y -q install \
   usg \
 
 #install nodejs using nvm
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/{$NVM_VERSION}/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/${NVM_VERSION}/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
-nvm install "{$NODE_VERSION}"
+nvm install "${NODE_VERSION}"
 npm install minimist
 npm install semver
 
