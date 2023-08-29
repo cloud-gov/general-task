0d51062230eb22e7408d0fd523ce3231b3b7077a
James Hochadel <james.hochadel@gsa.gov>
2023-08-29 09:24:25 -0400

    Fix syntax error

--- scripts/build.sh
+++ scripts/build.sh
@@ -201,7 +201,7 @@ echo "Installing GitHub CLI"
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg \
   | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main"
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" \
   | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 apt update
 apt install gh -y
