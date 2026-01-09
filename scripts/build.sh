92afd5d67dfb7250078b0b962762895a2a373541
David Anderson <david.n.anderson@gsa.gov>
2026-01-09 12:45:43 -0500

    update npm and remove old ruby gem

--- scripts/build.sh
+++ scripts/build.sh
@@ -95,6 +95,9 @@ ln -s /usr/local/lib/nodejs/node-v${NODE_VERSION}-linux-x64/bin/npm /usr/bin/npm
 ln -s /usr/local/lib/nodejs/node-v${NODE_VERSION}-linux-x64/bin/npx /usr/bin/npx
 rm -f "node-v${NODE_VERSION}-linux-x64.tar.xz"
 
+#update npm
+npm install -g npm@latest
+
 #install nvm for other node versions
 curl -s -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
@@ -136,13 +139,14 @@ gem install rdoc -v $RDOC_RELEASE_VERSION -q --silent
 # Install CGI
 gem install cgi -v $CGI_RELEASE_VERSION -q --silent
 
-#Install rexml
+# Install rexml
 gem install rexml -v $REXML_RELEASE_VERSION -q --silent
 
 # Install uaac gem
 gem install cf-uaac -v $UAAC_CLI_GEM_VERSION -q --silent
 
-update-ca-certificates
+# uninstall old rexml
+gem uninstall rexml -v 3.4.0
 
 echo "Installing Spruce"
 wget -q -L -O /usr/local/bin/spruce "https://github.com/geofffranks/spruce/releases/latest/download/spruce-linux-amd64"
